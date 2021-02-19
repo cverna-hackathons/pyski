fb42f5f1239ed121491af44b4a4ceecbe0482fa3
Peter Berezny <spontain@gmail.com>
2021-02-19 09:33:15 +0100

    transformed home route

---
+++ server/routes/HomeController.ts
@@ -0,0 +1,18 @@
+import {
+  Controller, Get, Response
+} from '@decorators/express';
+import {
+  Response as ExpressResponse
+} from 'express';
+
+@Controller('/')
+export class HomeController {
+  @Get('/')
+  public async home(
+    @Response() res: ExpressResponse
+  ) {
+    res.render('index', {
+      title: 'Pyski'
+    });
+  }
+}
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,16 +1,20 @@
 import { playPost } from './playPost'
 import type { Request, Application, Response } from 'express'
+import {
+  attachControllers
+} from '@decorators/express';
+import { HomeController } from './HomeController'
+
 
 export const Router = (app: Application) => {
   const GRID_SIZES = [10, 30, 50, 100]
 
-  app.get('/', (_req: Request, res: Response) => {
-    res.render('index', { title: 'Pyski server' })
-  })
+  attachControllers(app, [ HomeController ])
 
   app.get('/setup', (_req: Request, res: Response) => {
     res.render('play/setup', { GRID_SIZES })
   })
 
   app.post('/play', playPost)
+
 }
