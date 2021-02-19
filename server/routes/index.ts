cc8e1bbcdbe520ad0ce198d603f8de813c796549
Peter Berezny <spontain@gmail.com>
2021-02-19 10:02:42 +0100

    add decorated controllers

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,20 +1,14 @@
-import { playPost } from './playPost'
-import type { Request, Application, Response } from 'express'
+import type { Application } from 'express'
 import {
   attachControllers
 } from '@decorators/express';
 import { HomeController } from './HomeController'
+import { PlayController } from './PlayController';
 
 
 export const Router = (app: Application) => {
-  const GRID_SIZES = [10, 30, 50, 100]
-
-  attachControllers(app, [ HomeController ])
-
-  app.get('/setup', (_req: Request, res: Response) => {
-    res.render('play/setup', { GRID_SIZES })
-  })
-
-  app.post('/play', playPost)
-
+  attachControllers(app, [
+    HomeController,
+    PlayController
+  ])
 }
