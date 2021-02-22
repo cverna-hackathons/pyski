d0e529381bcabe375276e20d15fad381802cb5e5
Peter Berezny <spontain@gmail.com>
2021-02-22 15:47:05 +0100

    mixing

---
+++ client/src/components/MatchSetupForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <form action="/match" method="post">
+      <p>
+        <label>
+          Grid width:
+          <select name="grid_width" v-model="gridWidthInput">
+            <SizeOption
+              v-for="size in GRID_SIZES"
+              :size="size"
+              :key="`gw-${size}`"
+            />
+          </select>
+        </label>
+      </p>
+      <p>
+        <label>
+          Grid height:
+          <select name="grid_height" v-model="gridHeightInput">
+            <SizeOption
+              v-for="size in GRID_SIZES"
+              :size="size"
+              :key="`gh-${size}`"
+            />
+          </select>
+        </label>
+      </p>
+      <button>Let's play</button>
+    </form>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SizeOption from './SizeOption'
+
+const GRID_SIZES: number[] = [10, 30, 50, 100]
+export default defineComponent({
+  data() {
+    return {
+      GRID_SIZES,
+      gridWidthInput: GRID_SIZES[0],
+      gridHeightInput: GRID_SIZES[0],
+    }
+  },
+  components: {
+    SizeOption,
+  },
+})
+</script>
--- client/src/views/MatchSetup.tsx
+++ client/src/views/MatchSetup.tsx
@@ -1,10 +1,16 @@
 import { defineComponent } from 'vue'
+import MatchSetupForm from '../components/MatchSetupForm.vue'
+
 export default defineComponent({
+  components: {
+    MatchSetupForm,
+  },
   render() {
     return (
       <div>
         <h1>Match setup</h1>
         <p>Let's set a match up!</p>
+        <MatchSetupForm />
       </div>
     )
   },
