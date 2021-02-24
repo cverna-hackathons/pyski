f01045464c9f65bc24019ed36c63fe806653e944
Peter Berezny <spontain@gmail.com>
2021-02-24 17:57:30 +0100

    working the minimax algo

--- server/lib/players/maximus.js
+++ server/lib/players/maximus.js
@@ -6,10 +6,23 @@ const isOtherPlayerMark = (mark, myMark) => (
   mark !== EMPTY_MARK && myMark !== mark
 )
 
+const copyGrid = src => src.map(row => row.slice(0))
+const checkWinning = (grid, mark) => {
+
+}
+
 const minimax = (grid, mark, depth, isMaximing = true) => {
   let score = 0
+  let move
   grid.forEach((row, y) => row.forEach((value, x) => {
+    if (value === EMPTY_MARK) {
+      const newGrid = copyGrid(grid)
+      newGrid[y][x] = mark
 
+      if (checkWinning(newGrid, mark)) {
+        return { grid: newGrid,  }
+      }
+    }
   }))
 
   return score
