7138a44316460f7fd2f428c16ed9842b90b3bc80
Peter Berezny <spontain@gmail.com>
2021-03-15 14:38:34 +0100

    fixing CI

--- server/lib/config/index.ts
+++ server/lib/config/index.ts
@@ -1,14 +1,3 @@
 import * as dotenv from 'dotenv';
-import { resolve } from 'path';
 
-const configFileName = (
-  process.env.CI === 'true'
-    ? 'ci.env'
-    : '.env'
-)
-
-export const config = dotenv.config({
-  path: resolve(
-    __dirname, `../../${configFileName}`
-  ),
-});
+export const config = dotenv.config();
