cf268ac77a60ea6e9cafeacf1fea498cf4903ae7
Peter Berezny <spontain@gmail.com>
2021-03-18 09:10:30 +0100

    pwd util

--- server/lib/utils/password.ts
+++ server/lib/utils/password.ts
@@ -10,7 +10,13 @@ export type EncryptionInput = {
   salt: string;
 };
 
-const getEncryptedPassword = promisify((
+const BLOCK_SIZE = 8;
+const PARALLELIZATION = 2;
+const COST = 16384;
+const KEY_LENGTH = 64;
+const SALT_BYTE_LEN = 16;
+
+const _encrypt = promisify((
   {
     blockSize,
     keyLength,
@@ -25,17 +31,27 @@ const getEncryptedPassword = promisify((
   parallelization,
 }, (error, derivedKey) => done(error, derivedKey.toString('hex'))))
 
-export async function encrypt(password: string): Promise<string> {
-  const input: EncryptionInput = {
-    blockSize: 1,
-    keyLength: 64,
-    parallelization: 1,
-    password,
-    salt: 'full',
-  };
-  const pwdHash = await getEncryptedPassword(input);
 
-  // return [ pwdHash ]
+
+export async function encrypt(password: string): Promise<string> {
+  const salt = Crypto.randomBytes(SALT_BYTE_LEN).toString('hex');
+  const input: EncryptionInput = {
+    N: COST,
+    blockSize: BLOCK_SIZE,
+    keyLength: KEY_LENGTH,
+    parallelization: PARALLELIZATION,
+    password,
+    salt,
+  };
+  const pwdHash = await _encrypt(input);
+
+  return [
+    COST.toString(16),
+    BLOCK_SIZE.toString(16),
+    PARALLELIZATION.toString(16),
+    salt,
+    pwdHash,
+  ].join('$');
 }
 
 export async function verify(hash: string, password: string) {
@@ -43,14 +59,13 @@ export async function verify(hash: string, password: string) {
   const N = parseInt(n, 16)
   const blockSize = parseInt(v, 16)
   const keyLength = (pwdHash.length / 2)
-  const passwordText = `${password}`
   const parallelization = parseInt(r, 16)
-  const encPwdHash = await getEncryptedPassword({
+  const encPwdHash = await _encrypt({
     blockSize,
     N,
     keyLength,
     parallelization,
-    password: passwordText,
+    password,
     salt: saltOnly,
   })
   const hashesMatching = !!(encPwdHash === pwdHash)
