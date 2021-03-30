7f18769fc398acd44ae2e660ccbf29ded9055aad
Jakub Kanitra <>
2021-03-30 15:59:04 +0200

    Update test

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -10,7 +10,7 @@ import { AuthorizationProvider } from './auth/Authorization';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" data-testid="main-wrapper">
       <ApolloProvider client={graphql}>
         <BrowserRouter>
           <AuthorizationProvider>
